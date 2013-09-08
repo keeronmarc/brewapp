4d6661051fd47999d3c298daa9ce154f35045860
Pickra <picker10024@yahoo.com>
2013-09-08 10:41:59 -0400

    everything i said in the issue from sept 7, 2013 goes here

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,13 @@
-console.log('\'Allo \'Allo!');
+$(document).ready(function(){
+
+	$('.modal-button').click(function(){
+		$('.modal').addClass('hidden');
+		$('.modal').hide();
+		// $('.modal').addClass('hide');
+	});
+
+	$('.big-button-container').click(function(){
+		$('.next-page').addClass('visible');
+	});
+
+}); 
